9a9e9f89bc2346d7c5f7e1b322fdc26cc1f67854
陈曦 <420194918@qq.com>
2018-09-25 17:35:15 +0800

    nsdate 扩展

--- CXDatePickerSwift/CXDatePickerSwift/CXDatePickerSwift/extension.swift
+++ CXDatePickerSwift/CXDatePickerSwift/CXDatePickerSwift/extension.swift
@@ -8,13 +8,71 @@
 
 import Foundation
 
-class NSDate_Extension: NSDate {
-    
-    open class func dateWithString(date dateString:NSString,format:NSString)->(NSDate) {
+
+extension NSDate {
+    /// 根据时间字符串返回 nsdate
+    /// - Parameters:
+    ///   - dateString:
+    ///   - format:
+    /// - Returns:
+    class func dateWithStr(date dateString:NSString,format:NSString)->(NSDate) {
         
-        
-        return NSDate.init()
+        let formatter:DateFormatter = DateFormatter.init()
+        formatter.dateFormat = format.length > 0 ? format as String : "yyyy-MM-dd HH:mm:ss" as String
+        let gmtDate:NSDate = formatter.date(from: dateString as String)! as NSDate
+        return gmtDate
     }
     
+    
+    /// 根据时间 date 返回字符串
+    ///
+    /// - Parameters:
+    ///   - date:
+    ///   - format:
+    /// - Returns:
+    class func stringwithDate(date:NSDate ,format:NSString)->NSString{
+        
+        let formatter:DateFormatter = DateFormatter.init()
+        formatter.dateFormat = format.length > 0 ? format as String : "yyyy-MM-dd HH:mm:ss" as String
+        let dateStr:NSString = formatter.string(from: date as Date) as NSString
+        return dateStr
+    }
+    
+    /// 根据日期返回星期几
+    ///
+    /// - Parameter date:
+    /// - Returns:
+    class func weekwithDate(date:NSDate)->NSString{
+        let weekDays:NSArray = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"]
+        let interval = Int(NSDate.init().timeIntervalSince1970)
+        let days = Int(interval/86400) // 24*60*60
+        let weekday = ((days + 4)%7+7)%7
+        return weekDays.object(at: weekday == 0 ? 7 : weekday) as! NSString
+    }
+    
+    
+    /// 根据 date 返回今天昨天明天
+    ///
+    /// - Parameter date:
+    /// - Returns:
+    class func compareDate(date:NSDate)->NSString{
+        let dateFormat:DateFormatter = DateFormatter.init()
+        dateFormat.dateFormat = "yyyy-MM-dd"
+        //        //今天
+        let toStr:NSString = dateFormat.string(from: NSDate.init() as Date) as NSString
+        //        //昨天
+        let yestterStr:NSString = dateFormat.string(from: NSDate.init(timeInterval: -24*3600, since: NSDate.init() as Date) as Date) as NSString
+        //        //明天时间
+        let tomorrowStr:NSString = dateFormat.string(from: NSDate.init(timeInterval: 24*3600, since: NSDate.init() as Date) as Date) as NSString
+        
+        let dateStr:NSString = dateFormat.string(from: date as Date) as NSString
+        
+        switch dateStr {
+        case toStr: return "今天"
+        case yestterStr: return "昨天"
+        case tomorrowStr: return "明天"
+        default: return "错误"
+        }
+    }
     
 }
